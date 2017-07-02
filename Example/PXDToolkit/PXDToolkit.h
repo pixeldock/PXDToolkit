8b13f8fd2d22e4de5eafa70ffb21eb71c7be3a26
Jörn Schoppe <joern@pixeldock.com>
2017-07-02 16:52:43 +0200

    Add Carthage support

---
+++ Example/PXDToolkit/PXDToolkit.h
@@ -0,0 +1,19 @@
+//
+//  PXDToolkit.h
+//  PXDToolkit
+//
+//  Created by Jörn Schoppe on 02.07.17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PXDToolkit.
+FOUNDATION_EXPORT double PXDToolkitVersionNumber;
+
+//! Project version string for PXDToolkit.
+FOUNDATION_EXPORT const unsigned char PXDToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PXDToolkit/PublicHeader.h>
+
+
